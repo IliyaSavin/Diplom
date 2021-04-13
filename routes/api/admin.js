eb8a722d3b23c74c07ef70218c120e2ec751cf2a
IliyaSavin <49229895+IliyaSavin@users.noreply.github.com>
2021-04-13 14:05:35 +0300

    + station list fix

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const config = require('../../config/db');
 const { Connection, Request } = require("tedious");
 const auth = require('../../middleware/auth');
+const xl = require('excel4node');
 
 router.post('/addUser', auth, async (req, res) => {
     const {login, password} = req.body;
@@ -16,11 +17,11 @@ router.post('/addUser', auth, async (req, res) => {
                 //console.log(err.message);
                 if (err.message.includes("already exists")) {
                     res.json({
-                        message: "login already exists"
+                        message: "Login already exists"
                     })
                 } else {
                     res.json({
-                        message: "password error"
+                        message: "Password error"
                     })
                 }
             } else {
@@ -51,48 +52,6 @@ async function createUserForLogin (login) {
         ecoConnection.execSql(request);
     });
 }
-
-/*var userConfig = {
-    authentication: {
-        options: {
-          userName: "testUser5", // update me
-          password: "adddaaaSS22" // update me
-        },
-        type: "default"
-      },
-      server: "ecosensors.database.windows.net", // update me
-      options: {
-        database: "EcoSensorsAzure", //update me
-        encrypt: true,
-        packetSize: 32768,
-        trustServerCertificate: true
-      }
-}
-
-router.post('/test', async (req, res) => {
-    var connection = new Connection(userConfig);
-    connection.connect();
-    connection.on('connect', function(err) {
-        var all = [];
-        request = new Request("select * from Station", function(err, rowCount, rows) {
-            connection.close();
-            if (err) {
-                res.status(500).send('Server error');
-            } else {
-                res.json(all);
-            }
-        });
-        request.on("row", columns => {
-            var row = {};
-            columns.forEach(column => {
-              row[column.metadata.colName] = column.value;
-            });
-            all.push(row);
-          });
-        connection.execSql(request);
-    })
-})*/
-
 
 router.post('/setWorkingStatus', auth, async (req, res) => {
     const {status} = req.body;
@@ -139,6 +98,76 @@ router.get('/config', auth, async (req, res) => {
         connection.execSql(request);
     })
 })
+
+
+router.get('/activityLog/', auth, async (req, res) => {
+    var url = req.query;
+    var connection = new Connection(config.user(req.user));
+    connection.connect();
+    connection.on('connect', function(err) {
+        var all = [];
+        let requestStr = `SELECT event_time, sys.fn_get_audit_file.action_id, name, succeeded, server_principal_name, statement, additional_information, client_ip, duration_milliseconds FROM sys.fn_get_audit_file
+                            (
+                            'https://ecoaudit.blob.core.windows.net/sqldbauditlogs/ecosensors/EcoSensorsAzure/SqlDbAuditing_Audit_NoRetention/2021-04-12/15_11_13_772_0.xel', default, default
+                            ) inner join sys.dm_audit_actions ON sys.fn_get_audit_file.action_id = sys.dm_audit_actions.action_id`;
+        let orderStr = "";
+        let userStr = "";
+        let errorStr = "";
+
+        if (url.user) {
+            userStr = ` WHERE server_principal_name='${url.user}'`;
+        }
+
+        if (url.onlyErrors) {
+            if (userStr == "") {
+                errorStr = "  WHERE succeeded=0";
+            } else {
+                errorStr = " and succeeded=0";
+            }
+        }
+
+        let whereStr = userStr + errorStr;
+        if (url.dateFrom) {
+            if (whereStr == "") {
+                whereStr = ` WHERE event_time >= '${url.dateFrom}'`;
+            } else {
+                whereStr += ` and event_time >= '${url.dateFrom}'`;
+            }
+        }
+
+        if (url.dateTo) {
+            if (whereStr == "") {
+                whereStr = ` WHERE event_time < '${url.dateTo}'`;
+            } else {
+                whereStr += ` and event_time < '${url.dateTo}'`;
+            }
+        }
+
+        if (url.order == "Up") {
+            orderStr = " ORDER BY event_time";
+        } else if (url.order == "Down") {
+            orderStr = " ORDER BY event_time DESC";
+        }
+
+        request = new Request(requestStr + whereStr + orderStr, function(err, rowCount, rows) {
+            connection.close();
+            if (err) {
+                console.log(err);
+                res.status(500).send('Server error');
+            } else {
+                res.json(all);
+            }
+        });
+        request.on("row", columns => {
+            var row = {};
+            columns.forEach(column => {
+              row[column.metadata.colName] = column.value;
+            });
+            all.push(row);
+          });
+        connection.execSql(request);
+    })
+});
 
 
 module.exports = router;
